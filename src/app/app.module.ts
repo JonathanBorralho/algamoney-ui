4a8dd43a96d5b94c7fadfb9077c58fb78fd98b10
JonathanBorralho <jonathan.borralho@gmail.com>
2020-03-31 15:36:12 -0300

    Protótipo da tela de pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,12 @@ import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -20,9 +26,10 @@ import { InputTextModule } from 'primeng/inputtext';
 
     ButtonModule,
     CardModule,
-    InputTextModule
+    InputTextModule,
+    TableModule
   ],
-  providers: [],
+  providers: [ {provide: LOCALE_ID, useValue: 'pt'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
