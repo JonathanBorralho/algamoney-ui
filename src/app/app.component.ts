341fb2a7ad391fa3866e35f6e85fa5e8fa7586ed
JonathanBorralho <jonathan.borralho@gmail.com>
2020-03-31 17:40:56 -0300

    Criação de navbar e protótipo do sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   preserveWhitespaces: true
 })
-export class AppComponent {
-  title = 'algamoney-ui';
-
-
+export class AppComponent implements OnInit {
+  items = [];
+  ngOnInit() {
+    this.items = [
+      {label: 'Lançamentos', icon: 'pi pi-fw pi-chart-bar'},
+      {label: 'Pessoas', icon: 'pi pi-fw pi-users'},
+      {label: 'Logout', icon: 'pi pi-fw pi-sign-out'}
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,22 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu';
 
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-
 registerLocaleData(localePt, 'pt');
+
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +36,9 @@ registerLocaleData(localePt, 'pt');
     CardModule,
     InputTextModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    SidebarModule,
+    MenuModule
   ],
   providers: [ {provide: LOCALE_ID, useValue: 'pt'} ],
   bootstrap: [AppComponent]
