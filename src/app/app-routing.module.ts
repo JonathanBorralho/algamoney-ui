2d2b876594c6a170529aae62da74cf50df705e29
JonathanBorralho <jonathan.borralho@gmail.com>
2020-04-02 16:11:01 -0300

    Revert "Protótipo da tela de cadastro de pessoa"
    This reverts commit 0da80915a8f340bf0c7c3564cccd356a347bee9f.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 
 const routes: Routes = [
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
   { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'pessoas/nova', component: PessoaCadastroComponent },
   { path: 'pessoas', component: PessoasPesquisaComponent },
   { path: '', redirectTo: '/lancamentos', pathMatch: 'full' }
 ];
